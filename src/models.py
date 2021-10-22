7195b505b4f2151c13be25cba893089f4b93a5f6
ch4rbo <baueratctu@gmail.com>
2021-10-22 19:52:14 +0000

    armo tablas

--- src/models.py
+++ src/models.py
@@ -2,11 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    planetas_favoritos = db.relationship('Favorito_planeta', backref='usuario', lazy=True)
+    personajes_favoritos = db.relationship('Favorito_people', backref='usuario', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +19,75 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Planeta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    rotation_period = db.Column(db.Integer, unique=False, nullable=False)
+    orbital_period = db.Column(db.Integer, unique=False, nullable=False)
+    diameter = db.Column(db.Integer, unique=False, nullable=False)
+    climate = db.Column(db.String(80), unique=False, nullable=False)
+    gravity = db.Column(db.String(80), unique=False, nullable=False)
+    terrain = db.Column(db.String(80), unique=False, nullable=False)
+    surface_water = db.Column(db.Integer, unique=False, nullable=False)
+    population = db.Column(db.Integer, unique=False, nullable=False)
+    usuarios = db.relationship('Favorito_planeta', backref='planeta', lazy=True)
+
+
+    def __repr__(self):
+        return '<Planeta %r>' % self.name
+
+    def serialize(self):
+        return {
+                # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    height = db.Column(db.Integer, unique=False, nullable=False)
+    mass = db.Column(db.Integer, unique=False, nullable=False)
+    hair_color = db.Column(db.String(120), unique=False, nullable=False)
+    skin_color = db.Column(db.String(80), unique=False, nullable=False)
+    eye_color = db.Column(db.String(80), unique=False, nullable=False)
+    birth_year = db.Column(db.String(80), unique=False, nullable=False)
+    gender = db.Column(db.String(80), unique=False, nullable=False)
+    usuarios = db.relationship('Favorito_people', backref='people', lazy=True)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+                # do not serialize the password, its a security breach
+        }
+
+
+class Favorito_planeta(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey("usuario.id"), unique=False, nullable=False)
+    planeta_id = db.Column(db.Integer, db.ForeignKey("planeta.id"), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<favorito_planeta %r>' % self.id
+
+    def serialize(self):
+        return {
+                # do not serialize the password, its a security breach
+        }
+
+
+class Favorito_people(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey("usuario.id"), unique=False, nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey("people.id"), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<favorito_people %r>' % self.id
+
+    def serialize(self):
+        return {
+                # do not serialize the password, its a security breach
+        }
+
